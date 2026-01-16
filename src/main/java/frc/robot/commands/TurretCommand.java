79e5ab16a8073b7899964486dcfa167d828711e5
tinyrice01 <widee135@gmail.com>
2026-01-16 17:36:53 -0500

    turret turning

--- src/main/java/frc/robot/commands/TurretCommand.java
+++ src/main/java/frc/robot/commands/TurretCommand.java
@@ -2,21 +2,26 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.Vision;
 
 public class TurretCommand extends Command {
     
     private final Turret m_turret;
-    private final double targetAngleDegrees;
+    private final Vision m_vision;
 
-    public TurretCommand(Turret m_turret, double targetAngleDegrees) {
+    public TurretCommand(Turret m_turret, Vision m_vision) {
         this.m_turret = m_turret;
-        this.targetAngleDegrees = targetAngleDegrees;
+        this.m_vision = m_vision;
         
         addRequirements(m_turret);
     }
 
     @Override
     public void initialize() {
+        double targetAngleDegrees = Math.atan2(m_vision.getYPose(), m_vision.getXPose()) * (180.0/Math.PI);
+        if (targetAngleDegrees < 0) {
+            targetAngleDegrees += 360;
+        }
         m_turret.setSetpoint(targetAngleDegrees);
     }
 
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,6 +4,7 @@ import org.photonvision.PhotonCamera;
 
 import frc.robot.Constants;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.measure.Distance;
 
 public class Vision {
     private PhotonCamera m_camera;
@@ -17,5 +18,13 @@ public class Vision {
         var target = result.getBestTarget();
         var transform = target.getBestCameraToTarget().plus(Constants.Vision.kCameraToRobotTransform);
         return new Pose2d(transform.getTranslation().toTranslation2d(), transform.getRotation().toRotation2d());
+    }
+
+    public double getXPose() {
+        return getPose().getX();
+    }
+
+    public double getYPose() {
+        return getPose().getY();
     }
 }
