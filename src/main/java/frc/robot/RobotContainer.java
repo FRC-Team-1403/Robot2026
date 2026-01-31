0401bbcc3d362bfb07f4c2cd64d32634a377bbff
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2026-01-30 20:56:55 -0500

    tuning

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -42,10 +42,10 @@ public class RobotContainer {
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
-   m_driverController.rightTrigger().whileTrue(new ShooterCommandRPM(m_shooter, 3000));
+   m_driverController.rightTrigger().whileTrue(new ShooterCommandRPM(m_shooter, 1500));
     // RPM
 
-   m_driverController.leftTrigger().whileTrue(new ShooterCommandPower(m_shooter, 0.55));
+   m_driverController.leftTrigger().whileTrue(new ShooterCommandPower(m_shooter, 0.5));
    //power
    
   }
