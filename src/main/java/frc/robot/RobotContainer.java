dc31082265fd9881d12c7e8d8039335e2d0cb11e
Anuraag3345 <146129998+Anuraag3345@users.noreply.github.com>
2026-02-20 21:38:21 -0500

    Auto stuff commented out need to check if works

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,45 +3,40 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot;
 
-import frc.robot.commands.IntakeCommand;
-import frc.robot.commands.ShooterCommand;
-import frc.robot.commands.TurretCommand;
-import frc.robot.subsystems.Indexer;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.IntakeWrist;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.ShooterHood;
-import frc.robot.subsystems.Spindexer;
-import frc.robot.subsystems.Turret;
-import frc.robot.vision.Vision;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+// import frc.robot.commands.auto.AutoHelper;
+// import frc.robot.swerve.SwerveSubsystem;
+// import frc.robot.swerve.TunerConstants;
 
 public class RobotContainer {
-  private final Turret m_turret;
-  private final Intake m_intake;
-  private final IntakeWrist m_intakeWrist;
-  private final Shooter m_shooter;
-  private final ShooterHood m_shooterHood;
-  private final Indexer m_indexer;
-  private final Spindexer m_spindexer;
-  private final Vision m_vision;
+  //private final SwerveSubsystem m_swerve;
 
   private final CommandXboxController m_driverController =
       new CommandXboxController(Constants.Driver.kDriverControllerPort);
   private final CommandXboxController m_operatorController =
       new CommandXboxController(Constants.Operator.kOperatorControllerPort);
 
+  private LoggedDashboardChooser<Command> m_autoChooser;
+
   public RobotContainer() {
-    m_turret = new Turret();
-    m_intake = new Intake();
-    m_intakeWrist = new IntakeWrist();
-    m_shooter = new Shooter();
-    m_shooterHood = new ShooterHood();
-    m_indexer = new Indexer();
-    m_spindexer = new Spindexer();
-    m_vision = new Vision();
+    //m_swerve = TunerConstants.createDrivetrain();
+
+    if (AutoBuilder.isConfigured())
+      m_autoChooser = new LoggedDashboardChooser<Command>("Auto Chooser", AutoBuilder.buildAutoChooser());
+    else {
+      m_autoChooser = new LoggedDashboardChooser<Command>("Auto Chooser");
+      DriverStation.reportError("Auto builder wasn't configured!", true);
+    }
+
+    SmartDashboard.putData("Auto Chooser", m_autoChooser.getSendableChooser());
+
     configureBindings();
   }
 
@@ -55,9 +50,6 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    m_turret.setDefaultCommand(new TurretCommand(m_turret, m_vision));
-    m_operatorController.a().toggleOnTrue(new IntakeCommand(m_intake, m_intakeWrist));
-    m_operatorController.b().whileTrue(new ShooterCommand(m_shooter, m_indexer, m_spindexer, m_shooterHood, m_vision));
   }
 
   /**
@@ -66,6 +58,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return null;
+    return m_autoChooser.get();
   }
 }
---
+++ src/main/java/frc/robot/commands/auto/AutoHelper.java
@@ -0,0 +1,34 @@
+// package frc.robot.commands.auto;
+
+// import com.pathplanner.lib.auto.AutoBuilder;
+// import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.math.geometry.Rotation2d;
+// import edu.wpi.first.wpilibj.DriverStation.Alliance;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.Commands;
+// import frc.robot.util.AutoUtil;
+// import frc.robot.util.CougarUtil;
+// import frc.robot.swerve.SwerveSubsystem;
+// import frc.robot.swerve.TunerConstants;
+
+// public class AutoHelper {
+
+//     public static Command alignToStartingPose(SwerveSubsystem m_swerve, String pathName) {
+//         return m_swerve.defer(() -> {
+//             Pose2d target = AutoUtil.getStartingPose(pathName);
+//             if (target == null) return Commands.none();
+//             return AutoBuilder.pathfindToPose(target, TunerConstants.kPathConstraints);
+//         });
+//     }
+
+//     public static Command getMoveAuto(SwerveSubsystem m_swerve) {
+//         return m_swerve.defer(() -> {
+//             Pose2d target = m_swerve.getPose();
+//             if(CougarUtil.getAlliance() == Alliance.Red)
+//                 target = CougarUtil.addDistanceToPoseRot(target, Rotation2d.kZero, 1);
+//             else
+//                 target = CougarUtil.addDistanceToPoseRot(target, Rotation2d.k180deg, 1);
+//             return AutoBuilder.pathfindToPose(target, TunerConstants.kPathConstraints);
+//         });
+//     }
+// }
