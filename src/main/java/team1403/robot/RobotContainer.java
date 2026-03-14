c991b80919dc9371b4458cecf24fe8e044576f7a
meghan-ng <146130070+MishaGajula@users.noreply.github.com>
2026-03-14 00:29:31 -0400

    vibration command
    vibrates every time the hub active swiches per alliance

--- src/main/java/team1403/robot/RobotContainer.java
+++ src/main/java/team1403/robot/RobotContainer.java
@@ -5,10 +5,14 @@ package team1403.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import team1403.robot.commands.ControllerVibrationCommand;
 import team1403.robot.commands.InSpinShootCommand;
 import team1403.robot.commands.IntakeCommand;
 import team1403.robot.subsystems.Indexer;
@@ -31,6 +35,9 @@ public class RobotContainer {
   private final ShooterHood m_shooterHood;
   private final Turret m_turret;
 
+  //vibration command
+  private final Timer m_teleopTimer;
+
   @SuppressWarnings("all")
   private final CommandXboxController m_driverController =
       new CommandXboxController(Constants.Driver.kDriverControllerPort);
@@ -50,6 +57,10 @@ public class RobotContainer {
     m_shooterHood = new ShooterHood();
     m_turret = new Turret();
     
+    //for vibration command
+    m_teleopTimer = new Timer();
+
+    
     if (AutoBuilder.isConfigured())
       m_autoChooser =
           new LoggedDashboardChooser<Command>("Auto Chooser", AutoBuilder.buildAutoChooser());
@@ -59,6 +70,8 @@ public class RobotContainer {
     }
 
     SmartDashboard.putData("Auto Chooser", m_autoChooser.getSendableChooser());
+
+
 
     configureBindings();
   }
@@ -75,6 +88,25 @@ public class RobotContainer {
   private void configureBindings() {
     m_operatorController.a().toggleOnTrue(new IntakeCommand(m_intake, m_intakeWrist));
     m_operatorController.rightTrigger().whileTrue(new InSpinShootCommand(m_indexer, m_spindexer, m_shooter,1500,800,3000));
+
+
+    //vibration command - untested 
+    RobotModeTriggers.teleop().onTrue(Commands.runOnce(() -> {
+      m_teleopTimer.reset();
+      m_teleopTimer.start();
+      }
+    ));
+    double[] shiftTimes = {140, 125, 105, 80, 55, 30};
+    for (double shiftTime : shiftTimes) {
+      final double time = 150 - shiftTime; // convert "time remaining" to "time elapsed"
+      new Trigger(() -> DriverStation.isTeleopEnabled()
+        && m_teleopTimer.get() <= time + 0.5
+        && m_teleopTimer.get() >= time - 0.5)
+        .onTrue(Commands.parallel(
+            new ControllerVibrationCommand(m_driverController.getHID(), 0.6, 0.5).asProxy(),
+            new ControllerVibrationCommand(m_operatorController.getHID(), 0.6, 0.5).asProxy()
+        ));
+}
   }
 
   /**
