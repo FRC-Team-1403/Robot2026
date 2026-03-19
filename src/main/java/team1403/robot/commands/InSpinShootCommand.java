f66cec6c38c0492e4bf322ff8c6d9750a0f4e52b
Anuraag3345 <146129998+Anuraag3345@users.noreply.github.com>
2026-03-19 09:59:31 -0400

    Smart ddashboardfix

--- src/main/java/team1403/robot/commands/InSpinShootCommand.java
+++ src/main/java/team1403/robot/commands/InSpinShootCommand.java
@@ -1,5 +1,6 @@
 package team1403.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import team1403.robot.subsystems.Indexer;
 import team1403.robot.subsystems.Shooter;
@@ -36,21 +37,25 @@ public class InSpinShootCommand extends Command {
         m_shooterRPM = shooterRPM;
         m_hoodAngle = hoodAngle;
 
+        SmartDashboard.setDefaultNumber("InSpinShoot/IndexerRPM", m_indexerRPM);
+        SmartDashboard.setDefaultNumber("InSpinShoot/SpindexerRPM", m_spindexerRPM);
+        SmartDashboard.setDefaultNumber("InSpinShoot/ShooterRPM", m_shooterRPM);
+        SmartDashboard.setDefaultNumber("InSpinShoot/HoodAngle", m_hoodAngle);
 
-        addRequirements(indexer, spindexer, shooter,hood);
+        addRequirements(indexer, spindexer, shooter, hood);
     }
 
     @Override
     public void initialize() {
-        m_shooter.setFlywheelTargetRPM(m_shooterRPM);
-        m_shooterHood.setSetpoint(m_hoodAngle);
     }
 
     @Override
     public void execute() {
-        if (m_shooter.isFlywheelAtSpeed()&&m_shooterHood.atSetpoint()) {
-            m_indexer.setIndexerRPM(m_indexerRPM);
-            m_spindexer.setSpindexerRPM(m_spindexerRPM);
+        m_shooter.setFlywheelTargetRPM(SmartDashboard.getNumber("InSpinShoot/ShooterRPM", m_shooterRPM));
+        m_shooterHood.setSetpoint(SmartDashboard.getNumber("InSpinShoot/HoodAngle", m_hoodAngle));
+        if (m_shooter.isFlywheelAtSpeed() && m_shooterHood.atSetpoint()) {
+            m_indexer.setIndexerRPM(SmartDashboard.getNumber("InSpinShoot/IndexerRPM", m_indexerRPM));
+            m_spindexer.setSpindexerRPM(SmartDashboard.getNumber("InSpinShoot/SpindexerRPM", m_spindexerRPM));
         }
     }
 
