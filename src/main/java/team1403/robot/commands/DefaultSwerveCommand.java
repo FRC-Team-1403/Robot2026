64fbd9447d365b8a1f0ab30babb9f7862f635651
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2026-03-19 15:05:49 -0400

    swerve speeds

--- src/main/java/team1403/robot/commands/DefaultSwerveCommand.java
+++ src/main/java/team1403/robot/commands/DefaultSwerveCommand.java
@@ -46,7 +46,7 @@ public class DefaultSwerveCommand extends Command {
   private double prev_vertical = 0;
   private static final double kMaxVelocityChange = 5 * Constants.kLoopTime; //12
 
-  private double m_speedLimiter = 0.8;
+  private double m_speedLimiter = 0.4;
 
   private final ProfiledPIDController m_rotationPID = new ProfiledPIDController(5, 0, 0, 
     new TrapezoidProfile.Constraints(TunerConstants.kMaxAngularRate, 5)); //13
