24fe8feda7a25520f21a3839ebc1d4b4928953ee
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2026-03-30 21:17:45 -0400

    tuning

--- src/main/java/team1403/robot/commands/LERPShooter.java
+++ src/main/java/team1403/robot/commands/LERPShooter.java
@@ -126,7 +126,7 @@ public class LERPShooter extends Command {
         double fieldAngleToGoal = Math.toDegrees(Math.atan2(projDeltaY, projDeltaX));
         double robotHeading = projectedPivot.getRotation().getDegrees();
         double turretAngle = MathUtil.inputModulus(
-                fieldAngleToGoal - robotHeading - 82, // Changed from -90 to -82
+                fieldAngleToGoal - robotHeading - 90,
                 Constants.Turret.kMinAngleDegrees,
                 Constants.Turret.kMaxAngleDegrees
         );
