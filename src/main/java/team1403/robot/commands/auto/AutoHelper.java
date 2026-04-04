5d9fccd48d9afd0d824c174377819a34e03f9e24
Anuraag3345 <146129998+Anuraag3345@users.noreply.github.com>
2026-04-04 19:49:29 -0400

    Bump auto:)

--- src/main/java/team1403/robot/commands/auto/AutoHelper.java
+++ src/main/java/team1403/robot/commands/auto/AutoHelper.java
@@ -232,4 +232,48 @@ public static Command getLeftTrenchDoubleSweepDepot(SwerveSubsystem m_swerve) {
             return Commands.none();
         }
     }
+
+    public static Command getLeftBumpDoubleSweepDepot(SwerveSubsystem m_swerve) {
+        try {    
+            return Commands.sequence(
+                Commands.race(
+                    Commands.parallel(
+                        AutoUtil.loadPathPlannerPath("LeftBumpDoubleSweepDepotPt1", m_swerve, true),
+                        NamedCommands.getCommand("IntakeWrist Down Command")
+                    ),
+                    NamedCommands.getCommand("Intake Command")
+                ),
+                Commands.race(
+                    AutoUtil.loadPathPlannerPath("LeftBumpDoubleSweepDepotPt2", m_swerve, true),
+                    NamedCommands.getCommand("Intake Command")
+                ),
+                Commands.race(
+                    AutoUtil.loadPathPlannerPath("LeftBumpDoubleSweepDepotPt3", m_swerve, true),
+                    NamedCommands.getCommand("Shoot Command"),
+                    NamedCommands.getCommand("Wrist Wiggle Command")
+                ),
+                Commands.race(
+                    AutoUtil.loadPathPlannerPath("LeftBumpDoubleSweepDepotPt4", m_swerve, true),
+                    NamedCommands.getCommand("Intake Command")
+                ),
+                Commands.race(
+                    AutoUtil.loadPathPlannerPath("LeftBumpDoubleSweepDepotPt5", m_swerve, true),
+                    NamedCommands.getCommand("Shoot Command"),
+                    NamedCommands.getCommand("Wrist Wiggle Command")                    
+                ),
+                Commands.race(
+                    AutoUtil.loadPathPlannerPath("LeftBumpDoubleSweepDepotPt6", m_swerve, true),
+                    NamedCommands.getCommand("Shoot Command"),
+                    NamedCommands.getCommand("Intake Command")                    
+                )
+            );
+        } catch (Exception e) {
+            System.err.println("Could not load auto: " + e.getMessage());
+            return Commands.none();
+        }
+    }
+
+
+    
+
 }
