468abdcc67fce93521c0da8c7af1fb78202f9856
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2026-09-09 16:41:04 -0400

    drive slow

--- src/main/java/team1403/robot/commands/DefaultSwerveCommand.java
+++ src/main/java/team1403/robot/commands/DefaultSwerveCommand.java
@@ -139,13 +139,13 @@ public class DefaultSwerveCommand extends Command {
       if(operatorSniping || driverSniping){
           m_speedLimiter = 0.2;
       } else if(m_speedSupplier.getAsDouble()>0.3){
-          m_speedLimiter = 0.7;
+          m_speedLimiter = 0.2;
       } else {
-        m_speedLimiter = 0.5;
+        m_speedLimiter = 0.2;
       }
     } else {
       if(operatorSniping || driverSniping){
-          m_speedLimiter = 0.30;
+          m_speedLimiter = 0.2;
       } else if(m_speedSupplier.getAsDouble()>0.3){
           m_speedLimiter = 0.7;
       } else {
